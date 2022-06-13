a2e239124b02c09cb222adb56fe0a45823231a75
Catalin <94062067+petreacatalin@users.noreply.github.com>
2022-06-13 20:52:13 +0300

    Add files via upload

---
+++ Profiles/CategoryProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using MyStore.DTOs;
+using MyStore.Entities;
+
+namespace MyStore.Profiles
+{
+    public class CategoryProfile : Profile
+    {
+        public CategoryProfile()
+        {
+            CreateMap<Category, CategoryModel>().ReverseMap();
+            CreateMap<CustomerModel, Customer>();
+        }
+    }
+}
---
+++ Profiles/CustomerProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using MyStore.DTOs;
+
+namespace MyStore.Profiles
+{
+    public class CustomerProfile : Profile
+    {
+        public CustomerProfile()
+        {
+            CreateMap<Customer, CustomerModel>();
+            CreateMap<CustomerModel, Customer>()
+                .ForMember(dst => dst.CustomerOrders, map => map.Ignore());
+                
+        }
+    }   
+    
+}
